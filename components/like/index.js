5c9e88837c68679d19f7c2ddaa38444e5710a3b7
kai <kai@kai.com>
2020-03-03 20:55:23 +0800

    初始文件上传

---
+++ components/like/index.js
@@ -0,0 +1,24 @@
+// components/like/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    number: 9
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    OnLike () {
+    }
+  }
+})
