3fde76f9970ffcf87fd81c402fe7608ed64db377
邵琦凯 <kai@qq.com>
2020-03-04 17:16:33 +0800

    x

--- components/like/index.js
+++ components/like/index.js
@@ -4,21 +4,26 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    like: {
+      type: Boolean,
+      value: false
+    },
+    count: {
+      type: Number,
+      value: 0
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    number: 9
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    OnLike () {
-    }
+    OnLike() {}
   }
 })
